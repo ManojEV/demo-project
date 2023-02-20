8f05e071d4fe16895e54451ffe5117d9831ed40c
ManojEV <ev.manoj27@gmail.com>
2023-02-20 07:48:38 +0530

    python code updated

---
+++ pgm1.py
@@ -0,0 +1,7 @@
+
+dictionary1 = {"name": "Manoj", "age": 27}
+dictionary2 = {"name": "Manoj", "city": "Davangere"}
+
+merged_dict = {**dictionary1, **dictionary2}
+
+print("Merged dictionary is:", merged_dict)
