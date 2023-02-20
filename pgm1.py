1edfba747eaec1252eca480d7b43ef9879beec1e
ManojEV <ev.manoj27@gmail.com>
2023-02-20 07:54:09 +0530

    Updated code

--- pgm1.py
+++ pgm1.py
@@ -1,7 +1,4 @@
+# This program prints Hello, world!
 
-dictionary1 = {"name": "Manoj", "age": 27}
-dictionary2 = {"name": "Manoj", "city": "Davangere"}
+print('Hello, world!')
 
-merged_dict = {**dictionary1, **dictionary2}
-
-print("Merged dictionary is:", merged_dict)
